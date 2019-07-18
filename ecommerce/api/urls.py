0f5a43d41bb0ab3797ba47f642378c2948ee07f8
Ilya Koyfman <ilya.koy@outlook.com>
2019-07-18 06:42:53 -0400

    GET, POST, DELETE working

--- ecommerce/api/urls.py
+++ ecommerce/api/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 from api import views
 
 urlpatterns = [
-    # write your URL rules here
+    path('products/', views.ProductView.as_view()),
+    path('products/<int:id>', views.ProductView.as_view())
 ]
--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -10,13 +10,63 @@ from django.http import HttpResponse, JsonResponse
 class ProductView(View):
 
     def get(self, *args, **kwargs):
-        pass
+      
+        if kwargs != {}:
+            product = Product.objects.get(id=kwargs['id'])
+            product = serialize_product_as_json(product)
+            return JsonResponse(product,status=200)
+        else:
+            all_prods = Product.objects.all()
+            all_prods = [serialize_product_as_json(item) for item in all_prods]
+            return JsonResponse(all_prods,status=200,safe=False)
+
 
     def post(self, *args, **kwargs):
-        pass
+
+        try:
+            payload = json.loads(self.request.body)
+        except ValueError:
+            return JsonResponse(
+                {"success": False, "msg": "Provide a valid JSON payload"},
+                status=400)
+        
+        category = payload['category']
+        try:
+            cat_obj = Category.objects.get(id=category)
+        except:
+            return JsonResponse(
+                {"response":"Not a valid category"}, status=400
+            )
+
+        new_product = Product.objects.create(
+            name = payload['name'],
+            sku = payload['sku'],
+            description = payload['description'],
+            price = payload['price'],
+            category = cat_obj
+        )
+
+        data = serialize_product_as_json(new_product)
+
+        return JsonResponse(data,status=201)
 
     def delete(self, *args, **kwargs):
-        pass
+        if kwargs != {}:
+            try:
+                prod_id = kwargs['id']
+                Product.objects.get(id=prod_id).delete()
+                return JsonResponse(
+                    {"Response":"Product Id # {} delete".format(prod_id)},
+                    status=200
+                )
+                
+            except:
+                return JsonResponse(
+                    {"Product Not Found":"Can't find matching Id"},
+                     status=404,safe=False
+                     )
+        
+        return JsonResponse({},status=400,safe=False)
 
     def patch(self, *args, **kwargs):
         pass

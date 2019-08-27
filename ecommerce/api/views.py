8becbc2258e4cc31d51a3f9b81b251cc03cad22c
manofoar <joetynan@gmail.com>
2019-08-27 22:44:17 +0000

    all api functions appear to be working

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -6,20 +6,75 @@ from django.views.generic import View
 from django.http import HttpResponse, JsonResponse
 
 # complete the View below with all REST functionality
-
+#serialize_product_as_json()
 class ProductView(View):
-
+    
+    def _get_objects(self,product_id):
+        try:
+            return Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return None
     def get(self, *args, **kwargs):
-        pass
-
+        product_id = kwargs.get('product_id')
+        if product_id:
+            # detail
+            product = self._get_object(product_id)
+            if not product:
+                return JsonResponse(
+                    {"success": False, "msg": "Could not find product with id: {}".format(product_id)},
+                    status=404)
+            data = serialize_product_as_json(product)
+        else:
+            # list
+            qs = Product.objects.all()
+            data = [serialize_product_as_json(product) for product in qs]
+        return JsonResponse(data, status=200, safe=False)
+    
     def post(self, *args, **kwargs):
-        pass
-
+        if 'product_id' in kwargs:
+           return JsonResponse({"success": False, "msg": "Product already exists"}, status=400)
+        try:         
+            payload = json.loads(self.request.body)
+        except ValueError:
+            return JsonResponse({"success": False, "msg":"Provide a valid JSON payload"}, status=400)
+        
+        data = serialize_product_as_json(payload)
+        return JsonResponse(data, status=201, safe=False)
+            
     def delete(self, *args, **kwargs):
-        pass
+        product_id = kwargs.get('product_id')
+        if not product_id:
+            return JsonResponse({"success": False, "msg": "invalid HTTP method"}, status = 400)
+        product = self._get_object(product_id)
+        if not product:
+            return JsonResponse({success: False, "msg": "unable to find product with id {}".format(product_id)}, status = 404)
+        product.delete()
+        data = {"success":True}
+        return JsonResonse(data, status=200, safe=False)
+        
 
     def patch(self, *args, **kwargs):
-        pass
-
+        product_id = kwargs.get('product_id')
+        if not product_id:
+            return JsonResponse({"success": False, "msg": "invalid HTTP method"}, status = 400)
+        product = self._get_object(product_id)
+        if not product:
+            return JsonResponse({"success": False, "msg": "Product with id {} doesn't exist, bucko".format(product_id)}, status=404)
+        try:
+            payload = json.loads(self.request.body)
+        except ValueError:
+            return JsonResponse({"success":False, "msg": "Invalid JSON paylod there, chief"}, status = 400)
+        return self.update(people, payload, partiel=True)
+    
     def put(self, *args, **kwargs):
-        pass
+        product_id = kwargs.get('product_id')
+        if not product_id:
+            return JsonResponse({"success": False, "msg": "invalid HTTP method"}, status = 400)
+        product = self._get_object(product_id)
+        if not product:
+            return JsonResponse({"success": False, "msg": "Product with id {} doesn't exist, bucko".format(product_id)}, status=404)
+        try:
+            payload = json.loads(self.request.body)
+        except ValueError:
+            return JsonResponse({"success":False, "msg": "Invalid JSON paylod there, chief"}, status = 400)
+        return self.update(people, payload, partiel=True)
